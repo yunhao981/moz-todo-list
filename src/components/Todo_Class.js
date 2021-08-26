6f1f8a2867d835e152dfae56e4f6d73590c828e9
Yunhao Liu <yunhao981@hotmail.com>
2021-08-27 00:00:35 +0800

    updated w/ function components

---
+++ src/components/Todo_Class.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class TodoClass extends React.Component () {
+
+    render() {
+      return (
+        <li className="todo-stack-small">
+          <div className="c-cb">
+            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+            <label className="todo-label" htmlFor="todo-0">
+              {props.name}
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">{props.name}</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">{props.name}</span>
+            </button>
+          </div>
+        </li>
+
+      )
+    }
+   
+}
