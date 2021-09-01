132bca7d5c15625f9d2d66bc50a748b383ae7397
Yunhao Liu <yunhao981@hotmail.com>
2021-09-01 23:11:26 +0800

    i don't know why but it failed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import './App.css';
 import Todo from './components/Todo';
 import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
 function App(props) {
+  function addTask(name) {
+    console.log({name});
+  }
   const taskList = props.tasks.map(task => (
     <Todo
-     id={task.id}
-     name={task.name}
-     completed={task.completed}
-     key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
     />
   ));
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
     
-      <Form />
-    
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton /> 
+        <FilterButton /> 
+        <FilterButton /> 
       </div>
+
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,74 @@
-import React from "react"
+import React, { useState } from "react";
 
-export default function Form(props) {
-    return (
-        <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
+// class Form extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     alert("Hello World");
+//   }
+//   render(){
+//     return (
+//       <form>
+//       <h2 className="label-wrapper">
+//         <label htmlFor="new-todo-input" className="label__lg">
+//           What needs to be done?
+//         </label>
+//       </h2>
 
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
+//       <input
+//         type="text"
+//         id="new-todo-input"
+//         className="input input__lg"
+//         name="text"
+//         autoComplete="off"
+//       />
 
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-    )
+//       <button type="submit" className="btn btn__primary btn__lg">
+//         Add
+//       </button>
+//       </form>
+//    );
+//   }
+// }
+
+function Form(props) {
+  const [name, setName] = useState('Use hooks');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask("add task");
+    setName("");
+  }
+  return(
+    <form>
+      <h2 className="label-wrapper">
+       <label htmlFor="new-todo-input" className="label__lg">
+           What needs to be done?
+         </label>
+       </h2>
+
+       <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
+
+export default Form;
